1adc181770f642d4d95bf6e3967a0eae65adf9db
Zhiyu Liang <edward.liang@mail.utoronto.ca>
2017-05-17 12:12:37 -0400

    q19

---
+++ removeNthFromEnd.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        start.next = head;
+        ListNode slow = start, fast = start;
+        
+        // Move the fast so that the distance between slow and fast is n.
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
